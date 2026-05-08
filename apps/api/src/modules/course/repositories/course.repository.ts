f0f22c15f369126f831a5c5fc4d216d58783104b
Pasha Pechenyi <pashapechenn@gmail.com>
2026-05-08 21:45:40 +0300

    feat(api): add course delete endpoint

--- apps/api/src/modules/course/repositories/course.repository.ts
+++ apps/api/src/modules/course/repositories/course.repository.ts
@@ -2,14 +2,26 @@ import { Injectable } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { EntityNotFoundException } from '../../../common/exceptions/entity-not-found.exception';
 import { Course } from '../../../database/entities/course.entity';
+import { Lesson } from '../../../database/entities/lesson.entity';
 import { BaseRepository } from '../../../database/repositories/base.repository';
 import { CourseListItem } from '../structs/course-list-item.constructor';
 import { IFindCourseListParams } from '../structs/find-course-list-params.interface';
 
 @Injectable()
 export class CourseRepository extends BaseRepository<Course> {
-  constructor(dataSource: DataSource) {
+  constructor(private readonly dataSource: DataSource) {
     super(dataSource, Course);
+  }
+
+  // FIXME: check do we need it or just add cascade update/delete to migration
+  async deleteWithLessons(id: string): Promise<void> {
+    await this.dataSource.transaction(async (manager) => {
+      await manager.delete(Lesson, { CourseId: id });
+      const result = await manager.delete(Course, id);
+      if (!result.affected) {
+        throw new EntityNotFoundException({ entity: Course, ctx: { id } });
+      }
+    });
   }
 
   async getOneByIdWithRelationsOrFail(id: string): Promise<Course> {
