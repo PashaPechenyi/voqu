f97ba510782cd58d48319adf4e652f7bc7e5b290
Pasha Pechenyi <pashapechenn@gmail.com>
2026-05-18 10:59:54 +0300

    remove all frontend part

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,12 +1,7 @@
-import { BrowserRouter } from 'react-router-dom';
-import { AppRoutes } from './routes';
+import { Box } from '@mui/material';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
-  );
+  return <Box> HELLO! </Box>;
 }
 
 export default App;
