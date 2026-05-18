86b82d24f636a21416a34e50eaaec916255d73f4
Pasha Pechenyi <pashapechenn@gmail.com>
2026-05-18 11:40:15 +0300

    feat(review): update code

---
+++ apps/api/src/modules/course/structs/admin-course-details.constructor.ts
@@ -0,0 +1,49 @@
+import { Course } from '../../../database/entities/course.entity';
+import { CourseStatus } from './course-status.enum';
+
+export class AdminCourseDetails {
+  constructor(data: Course) {
+    this.id = data.id!;
+    this.name = data.name!;
+    this.description = data.description ?? null;
+    this.status = data.status!;
+    this.Level = {
+      id: data.Level!.id!,
+      name: data.Level!.name!,
+      cefrLevel: data.Level!.cefrLevel!,
+    };
+    this.Owner = {
+      id: data.Owner!.id!,
+      firstName: data.Owner!.firstName!,
+      lastName: data.Owner!.lastName!,
+      email: data.Owner!.email!,
+    };
+    this.createdAt = data.createdAt!;
+    this.updatedAt = data.updatedAt!;
+  }
+
+  id: string;
+
+  name: string;
+
+  description: string | null;
+
+  status: CourseStatus;
+
+  Level: {
+    id: string;
+    name: string;
+    cefrLevel: string;
+  };
+
+  Owner: {
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+  };
+
+  createdAt: string;
+
+  updatedAt: string;
+}
